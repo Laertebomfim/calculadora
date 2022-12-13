4e05abd9817f4fe6bc6690b830ea8922ebd674ca
Laerte Bomfim Lima <113030513+Laertebomfim@users.noreply.github.com>
2022-12-12 23:31:06 -0300

    inicio2

--- projetos/script.js
+++ projetos/script.js
@@ -12,7 +12,39 @@ const funcoes = [
         return v1/v2
     }
 ]
-var valores = [...document.querySelectorAll("tbody>tr>td")]
-valores.map((elemento,ind)=>{
-    console.log(elemento)
+function manipulação(){
+
+}
+
+function muda(...valor_class){
+    valor_class.map((elemen,ind)=>{
+         elemen.classlist.toggles("ola")
+         return pegaNU(onumero)
+    })
+    function pegaNU(valor){
+        var primeiroN= Number(valor)
+        caixa.value=primeiroN
+
+    }
+}
+
+var valores = [...document.getElementsByClassName("numero")]
+var manipula = [...document.getElementsByClassName("manipuladores")]
+var onumero = '' 
+const caixa= window.document.querySelector("thead>tr>th")
+
+valores.map((elemento,inds)=>{
+    elemento.addEventListener("click",()=>{
+        onumero += elemento.innerHTML
+        caixa.innerHTML=onumero
+    })
+})
+manipula.map((elemento,ind)=>{
+    elemento.addEventListener("click",()=>{
+    muda(...valores)
+
+    if(ind==0){
+
+    }
+    })
 })
